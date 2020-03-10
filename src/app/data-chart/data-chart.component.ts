a6f2b063966f2eb57b6c0d5ff6e4b11c8f8cc5a1
HongliangWang.mac <whl7770@gmail.com>
2020-03-10 14:22:13 +0800

    add sensor chart data input

--- src/app/data-chart/data-chart.component.ts
+++ src/app/data-chart/data-chart.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import * as Highcharts from "highcharts";
+import { ChartInit } from "../mutisensor-view/sensor-chart";
 
 declare var require: any;
 let Boost = require("highcharts/modules/boost");
@@ -16,51 +17,84 @@ noData(Highcharts);
   styleUrls: ["./data-chart.component.scss"]
 })
 export class DataChartComponent implements OnInit {
-  public options: any = {
-    chart: {
-      type: "scatter",
-      height: 500
-    },
-    title: {
-      text: ""
-    },
-    credits: {
-      enabled: false
-    },
-    tooltip: {
-      formatter: function() {
-        return (
-          "x: " +
-          Highcharts.dateFormat("%e %b %y %H:%M:%S", this.x) +
-          " y: " +
-          this.y.toFixed(2)
-        );
-      }
-    },
-    xAxis: {
-      type: "datetime",
-      labels: {
-        formatter: function() {
-          return Highcharts.dateFormat("%e %b %y", this.value);
-        }
-      }
-    },
-    series: [
-      {
-        name: "Normal",
-        turboThreshold: 500000,
-        data: [[new Date("2018-01-25 18:38:31").getTime(), 2]]
-      },
-      {
-        name: "Abnormal",
-        turboThreshold: 500000,
-        data: [[new Date("2018-02-05 18:38:31").getTime(), 7]]
-      }
-    ]
+  @Input() chartInit: ChartInit = {
+    //设置默认值
+    chartType: null,
+    chartHeight: null,
+    dateFormat: null,
+    seriesName: null
   };
+  public options: any;
+
   constructor() {}
 
   ngOnInit() {
+    //FAO:放到此处初始化,如果在声明的时候初始化,会出现INPUT尚未传值的情况.
+    this.options = {
+      chart: {
+        type: this.chartInit.chartType,
+        height: this.chartInit.chartHeight
+      },
+      title: {
+        text: ""
+      },
+      credits: {
+        enabled: false
+      },
+      tooltip: {
+        formatter: function() {
+          return this.y.toFixed(2);
+        }
+      },
+      xAxis: {
+        type: "datetime",
+        labels: {
+          formatter: function() {
+            return Highcharts.dateFormat("%y-%m-%d %H:%M:%S", this.value);
+          }
+        }
+      },
+      yAxis: [
+        {
+          type: "CO2"
+        }
+      ],
+      series: [
+        {
+          name: "二氧化碳浓度",
+          turboThreshold: 500000,
+          data: [
+            [new Date("2018-01-25 18:38:31").getTime(), 480],
+            [new Date("2018-01-25 19:38:31").getTime(), 480]
+          ]
+        },
+        {
+          name: "温度",
+          turboThreshold: 500000,
+          data: [
+            [new Date("2018-01-25 18:38:31").getTime(), 18],
+            [new Date("2018-01-25 19:38:31").getTime(), 480]
+          ]
+        },
+        {
+          name: "湿度",
+          turboThreshold: 500000,
+          data: [
+            [new Date("2018-01-25 18:38:31").getTime(), 68],
+            [new Date("2018-01-25 19:38:31").getTime(), 480]
+          ]
+        },
+        {
+          name: "PM2.5浓度",
+          turboThreshold: 500000,
+          data: [
+            [new Date("2018-01-25 18:38:31").getTime(), 32],
+            [new Date("2018-01-25 19:38:31").getTime(), 480]
+          ]
+        }
+      ]
+    };
+
     Highcharts.chart("container", this.options);
   }
 }
--- src/app/mutisensor-view/mutisensor-view.component.ts
+++ src/app/mutisensor-view/mutisensor-view.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ChartInit } from './sensor-chart';
 
 @Component({
-  selector: 'app-mutisensor-view',
-  templateUrl: './mutisensor-view.component.html',
-  styleUrls: ['./mutisensor-view.component.scss']
+  selector: "app-mutisensor-view",
+  templateUrl: "./mutisensor-view.component.html",
+  styleUrls: ["./mutisensor-view.component.scss"]
 })
 export class MutisensorViewComponent implements OnInit {
+  public sensorChartInit : ChartInit = {
+    chartType: 'scatter',
+    chartHeight: 500,
+    dateFormat: '%y-%m-%d %H:%M:%S',
+    seriesName:['温度','湿度','二氧化碳浓度','PM2.5浓度'],
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
