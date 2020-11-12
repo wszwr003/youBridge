7ff4c7ee426fe918ce369c881453816cc827c913
HongliangWang.mac <whl7770@gmail.com>
2020-11-12 16:41:04 +0800

    raw video  success

--- src/app/live-video/live-video.component.ts
+++ src/app/live-video/live-video.component.ts
@@ -1,37 +1,51 @@
 import { Component, OnInit } from "@angular/core";
+import videojs from "video.js";
 
+declare var require: any;
+require("videojs-contrib-quality-levels");
+require("videojs-hls-quality-selector");
 @Component({
   selector: "app-live-video",
   templateUrl: "./live-video.component.html",
   styleUrls: ["./live-video.component.scss"],
 })
 export class LiveVideoComponent implements OnInit {
+  public player: videojs.Player;
+  public sliderVideos = [
+    [
+      "https://mdbootstrap.com/img/video/Tropical.mp4",
+      "Tropical",
+      "mdbootstrap.com/img/video/Tropical",
+    ],
+  ];
+  url =
+    "https://hls01open.ys7.com/openlive/d87fa54269d34203910b00aa1198d735.m3u8";
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  public sliderVideos = [
-    ["./assets/Tropical.mp4", "Tropical", "mdbootstrap.com/img/video/Tropical"],
-    ["./assets/forest.mp4", "forest", "mdbootstrap.com/img/video/forest"],
-    [
-      "./assets/Agua-natural.mp4",
-      "Agua-natural",
-      "mdbootstrap.com/img/video/Agua-natural",
-    ],
-    // [
-    //   "https://mdbootstrap.com/img/video/Agua-natural.mp4",
-    //   "Agua-natural",
-    //   "mdbootstrap.com/img/video/Agua-natural",
-    // ],
-    // [
-    //   "https://mdbootstrap.com/img/video/Tropical.mp4",
-    //   "Tropical",
-    //   "mdbootstrap.com/img/video/Tropical",
-    // ],
-    // [
-    //   "https://mdbootstrap.com/img/video/forest.mp4",
-    //   "forest",
-    //   "mdbootstrap.com/img/video/forest",
-    // ],
-  ];
+  ngAfterViewInit() {
+    const options = {
+      sources: [
+        {
+          src: this.url,
+          type: "application/x-mpegURL",
+        },
+      ],
+      // 'poster' : this.urlPoster
+    };
+    this.player = videojs("my-video", options, function onPlayerReady() {
+      console.log("Player ready");
+      var myPlayer = this,
+        id = myPlayer.id();
+      myPlayer.hlsQualitySelector();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.player != null) {
+      this.player.dispose();
+    }
+  }
 }
